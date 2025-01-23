39543defe1006683342ba0989a79108a2e478e13
Jedsada Tiwongvorakul <jedsada.gm@gmail.com>
2025-01-23 10:49:39 +0700

    test: build and start for production mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const withTM = require("next-transpile-modules")(["nuvo-react"]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig);
